96edee2d16635c2f06a4209f43764dcffd678d72
lpel3s <tut08480@temple.edu>
2024-12-09 02:56:26 -0800

    sf

---
+++ Farming on the RIm/Source/Tranquilizer/Class2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace LAP
+{
+    public class Projectile_TranquilizingBullet : Bullet
+    {
+        public ThingDef_TranquilizingBullet Def
+        { get
+            {
+                return this.def as ThingDef_TranquilizingBullet;
+            }
+        }
+    protected override void Impact(Thing hitThing, bool blockedByShield = false)
+        {
+            base.Impact(hitThing, blockedByShield);
+            if (Def != null && hitThing is Pawn hitpawn && hitThing != null)
+            {
+                var catatonicpawn = hitpawn?.health?.hediffSet?.GetFirstHediffOfDef(Def.HediffToAdd);
+                if (catatonicpawn != null)
+                {
+                    Hediff hediff = HediffMaker.MakeHediff(Def.HediffToAdd, hitpawn, null);
+                    hitpawn.health.AddHediff(hediff, null, null);
+                }
+            }
+                
+
+            }
+        }
+    }
