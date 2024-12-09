a7af1ac95c5a6e2d7f8586ec64c2bbe99b65a470
lpel3s <tut08480@temple.edu>
2024-12-09 00:41:16 -0800

    added mod

---
+++ Farming on the RIm/Source/Healing Gun/ThingDef_HealingBullet.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP
+{
+    public class ThingDef_HealingBullet : ThingDef 
+    {
+        public HediffDef HediffToRemove = HediffDefOf.BloodLoss;
+    }
+}
---
+++ Farming on the RIm/Source/Toxifruit/IngestionOutcomeDoer_RemoveHediff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace PL
+{
+    public class IngestionOutcomeDoer_RemoveHediff : IngestionOutcomeDoer
+    {
+        public HediffDef removeThisThing;
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int Thing)
+        {
+            var toxicHediff = pawn.health.hediffSet.GetFirstHediffOfDef(removeThisThing);
+            if (toxicHediff != null)
+            {
+                pawn.health.hediffSet.hediffs.Remove(toxicHediff);
+            }
+        }
+    }
+}
